c1b184378db1356ede3293c733a066f28da9e092
Jarmo Vleeshouwer <j.vleeshouwer@youweagency.com>
2025-01-24 20:38:39 +0100

    Improve table of contents

--- components/molecules/TableOfContents.tsx
+++ components/molecules/TableOfContents.tsx
@@ -15,14 +15,15 @@ const TableOfContents: FC<TableOfContentsProps> = ({ headers }) => {
     return (
         <div className="hidden desktop:flex flex-col  bg-primary-lighter rounded-xl py-md px-lg">
             <span className="font-noto text-[22px] mb-sm">Inhoudsopgave</span>
+
             {headers.map((header) => (
                 <Link
                     href={`#${header.identifier}`}
                     key={header.identifier}
                     className={`text-green-primary hover:underline ${
                         header.level === 3
-                            ? 'pl-4 text-xs mb-xs'
-                            : 'test-sm mb-[8px]'
+                            ? 'pl-4 text-xs mb-xs border-l border-l-primary-light'
+                            : 'text-xs my-xs leading-5'
                     }`}
                 >
                     {header.text}
