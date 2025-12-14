ca90e2e1ecbc4f50d3e16abd973975509be92c37
Jarmo Vleeshouwer <j.vleeshouwer@youweagency.com>
2025-12-14 09:29:38 +0100

    Missing locale fix

--- app/[locale]/posts/page.tsx
+++ app/[locale]/posts/page.tsx
@@ -2,35 +2,33 @@ import Container from '@/atoms/Container'
 import { getBlogPosts } from '@/client/contentful/BlogApi'
 
 import PostTile from '@/molecules/PostTile'
-import React, { FC, Suspense } from 'react'
+import React from 'react'
 import Header from '@/organisms/Header'
+import { getContentFullLocale } from '@/utils/locales'
+import { getTranslations } from 'next-intl/server'
 
-export function generateMetadata() {
+export async function generateMetadata() {
+    const t = await getTranslations('Metadata')
     return {
-        title: `Overzicht van alle reisartikelen | The Active Sloth`,
-        description: `Reis, relax repeat! De leukste reisartikelen vind je op The Active Sloth`,
+        title: t('posts-title'),
+        description: t('posts-description'),
     }
 }
 
-const AllBlogPosts: FC = async () => {
-    const posts = await getBlogPosts()
-
-    return (
-        <div className="grid gap-sm grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3">
-            {posts.map((post) => (
-                <PostTile key={post.id} post={post} />
-            ))}
-        </div>
-    )
-}
-
-export default async function PostsPage() {
+export default async function PostsPage({ params }: PageProps<'/[locale]'>) {
+    const { locale } = await params
+    const contentfulLocale = getContentFullLocale(locale)
+    const posts = await getBlogPosts(contentfulLocale)
     return (
         <Container>
             <Header withBorder />
             <div className="mt-md">
                 <h2 className={`text-2xl font-noto mb-md`}>Alle artikelen</h2>
-                <AllBlogPosts />
+                <div className="grid gap-sm grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3">
+                    {posts.map((post) => (
+                        <PostTile key={post.id} post={post} />
+                    ))}
+                </div>
             </div>
         </Container>
     )
